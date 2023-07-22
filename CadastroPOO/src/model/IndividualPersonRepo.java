8f96ce945addb33b551f65d6534884ee9cac3175
Debora Damas <damasdebora32@gmail.com>
2023-07-22 12:40:28 -0300

    4 c

--- CadastroPOO/src/model/IndividualPersonRepo.java
+++ CadastroPOO/src/model/IndividualPersonRepo.java
@@ -27,4 +27,8 @@ public class IndividualPersonRepo {
     
     public void getAll(){
     }
+    
+    public void persist(String file){
+        
+    }
 }
--- CadastroPOO/src/model/LegalPersonRepo.java
+++ CadastroPOO/src/model/LegalPersonRepo.java
@@ -27,4 +27,8 @@ public class LegalPersonRepo {
     
     public void getAll(){
     }
+    
+    public void persist(String file){
+        
+    }
 }
