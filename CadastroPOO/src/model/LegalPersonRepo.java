d2eade8aa0587fe8b771a1da6c4b833595da980d
Debora Damas <damasdebora32@gmail.com>
2023-07-22 12:27:25 -0300

    4 b

---
+++ CadastroPOO/src/model/LegalPersonRepo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author debora
+ */
+public class LegalPersonRepo {
+   private final ArrayList<LegalPerson> legalList = new ArrayList<LegalPerson>();
+       
+    public void insert(){
+    }
+    
+    public void change(){   
+    }
+    
+    public void delete(){
+    }
+    
+    public void get(){
+    }
+    
+    public void getAll(){
+    }
+}
