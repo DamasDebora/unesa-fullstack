3516f89cdf43bc3d3acbd969feeab61d5193680e
Debora Damas <damasdebora32@gmail.com>
2023-07-22 12:24:21 -0300

    4 a

---
+++ CadastroPOO/src/model/IndividualPersonRepo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author debora
+ */
+public class IndividualPersonRepo {
+    private final ArrayList<IndividualPerson> individualList = new ArrayList<IndividualPerson>();
+    
+    public void insert(){
+    }
+    
+    public void change(){   
+    }
+    
+    public void delete(){
+    }
+    
+    public void get(){
+    }
+    
+    public void getAll(){
+    }
+}
