fc48880a2570f6538d94cc75466592bc2cbebb1f
Debora Damas <damasdebora32@gmail.com>
2023-07-22 12:07:02 -0300

    LegalPerson class

---
+++ CadastroPOO/src/model/LegalPerson.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author debora
+ */
+public class LegalPerson extends Person{
+    private String cnpj;
+    
+    public LegalPerson(String cnpj, int id, String name){
+        super(id, name);
+        this.cnpj = cnpj;
+    }
+    
+    public String getCnpj(){
+        return cnpj;
+    }
+    
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+    
+    @Override 
+    public void show(){
+        System.out.print(cnpj);
+    }
+}
